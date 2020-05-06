bdcc469945a69a464ccb8e232b5c6d05b49cef1a
Tomasz Klara <tomaszklara@gmail.com>
2020-05-06 12:34:55 +0100

    updates

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -4,8 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../public/src/css/main.css">
-  <link rel="stylesheet" href="../public/src/css/uikit.min.css">
+  <link rel="stylesheet" href="<?php echo URLROOT ?>/public/src/css/main.css">
+  <link rel="stylesheet" href="<?php echo URLROOT ?>/public/src/css/uikit.min.css">
   <title><?php echo SITENAME; ?></title>
 </head>
 
